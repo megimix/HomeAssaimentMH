2681d6664088f108256fa6db78235eac7fca2a07
Tal Shachar <tal.shachar@houzz.com>
2023-01-29 11:57:35 +0200

    Initial Commit

---
+++ HomeAssaimentMH/HomeAssaimentMHApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAssaimentMHApp.swift
+//  HomeAssaimentMH
+//
+//  Created by Tal Shachar on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeAssaimentMHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
